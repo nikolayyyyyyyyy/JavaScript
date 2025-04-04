c5a7233fc970d7e032129cea21655893cefaab7c
Nikolay Nikolaev <cholyo999999@gmail.com>
2025-04-04 11:27:32 +0300

    submit

--- DOM_advanced_js/01. Subtraction/subtraction.js
+++
@@ -1,3 +0,0 @@
-function subtract() {
-    //TODO
-}
--- DOM_advanced_js/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ DOM_advanced_js/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,32 @@
 function solve() {
-  //TODO
+  debugger
+  const textElement = [...document.querySelector('#text')
+    .value
+    .toLowerCase()
+    .split(" ")];
+
+  const convention = document
+    .querySelector('#naming-convention').value;
+
+  const result = document
+    .querySelector('#result');
+
+  if (convention === 'Camel Case') {
+
+    result.textContent = textElement.map((e, i) => {
+      if (i === 0) {
+        return e.charAt(0).toLowerCase() + e.slice(1).toLowerCase();
+      } else {
+        return e.charAt(0).toUpperCase() + e.slice(1).toLowerCase();
+      }
+    }).join("");
+  } else if (convention === 'Pascal Case') {
+
+    result.textContent = textElement
+      .map(e => e.charAt(0).toUpperCase() + e.slice(1))
+      .join("");
+  } else {
+
+    result.textContent = "Error!"
+  }
 }
